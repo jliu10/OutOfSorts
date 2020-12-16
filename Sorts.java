6f324cb1bb2a920fe8dc7234d371bf7401e9d7fb
Justin Liu <exovitejl@gmail.com>
2020-12-16 16:42:43 -0500

    added class Sorts

---
+++ Sorts.java
@@ -0,0 +1,21 @@
+public class Sorts{
+  /**Bubble sort of an int array.
+  *@postcondition The array will be modified such that the elements will be in increasing order.
+  *@param data  the elements to be sorted.
+  */
+  public static boolean isSorted(int[] data){
+    return false;
+  }
+
+  public static void bubbleSort(int[] data){
+    // int n=data.length;
+    // for(int i=0; i<n; n--){
+    for(int n=data.length; n>0; n--){
+      if(isSorted) n=0;
+      else for(int i=0; i<n; i++){
+
+      }
+    }
+  }
+
+}
