8baf13a2df33eef279a2eced799ce639eac74d38
Justin Liu <exovitejl@gmail.com>
2020-12-16 17:13:11 -0500

    wrote helper method swap

--- Sorts.java
+++ Sorts.java
@@ -3,6 +3,12 @@ public class Sorts{
   *@postcondition The array will be modified such that the elements will be in increasing order.
   *@param data  the elements to be sorted.
   */
+  public static void swap(int[] data,int a,int b){
+      int t=data[a];
+      data[a]=data[b];
+      data[b]=t;
+  }
+
   public static void bubbleSort(int[] data){
     boolean sorted=false;
     if(data.length<=1) sorted=true;
@@ -11,10 +17,17 @@ public class Sorts{
     for(int n=data.length; n>0; n--){
       if(sorted) n=0;
       else for(int i=0; i<n-1; i++){
+        boolean madeSwap=false;
         if(data[i]>data[i+1]){
-
+          // int t=data[i];
+          // data[i]=data[i+1];
+          // data[i+1]=t;
+          swap(data,i,i+1);
+          madeSwap=true;
         }
       }
+      if()
+      if(!madeSwap) sorted=true;
     }
   }
 
